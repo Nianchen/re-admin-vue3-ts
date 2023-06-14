e9d9b8585fdcb00513172893497b3b377c9df0a8
Nianchen <2216092064@qq.com>
2023-06-14 09:34:22 +0800

    fix: 🐛 axios响应拦截器全局错误处理

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import type { AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios'
 import { useRouter } from 'vue-router'
+import { message } from 'ant-design-vue'
 // const request: AxiosInstance = axios.create({
 //     baseURL: "http://localhost:5555",
 //     timeout: 8000
@@ -22,11 +23,12 @@ axios.interceptors.request.use(
             config.headers['Content-Type'] = 'application/json';
         }
         //然后处理token逻辑
-        config.headers['Authorization'] = 'Bearer your-token';
+        config.headers['Authorization']= 'Bearer your-token';
+        console.log(config);
+        console.log("🚀 ~ file: request.ts:28 ~ config:", config)
         return config;
     },
     (error: any) => {
-        router.push({ name: "Login" })
         localStorage.clear()
         return Promise.reject(error);
     }
@@ -37,7 +39,51 @@ request.interceptors.response.use(
         if (status == 200)
             return data
     }, err => {
-        return Promise.reject(err)
+        //能在这里处理http常见错误
+        let Errmessage = '';
+        switch (err.response.status) {
+            case 400:
+                Errmessage = '请求错误(400)';
+                break;
+            case 401:
+                Errmessage = '未授权，请重新登录(401)';
+                // 这里可以做清空storage并跳转到登录页的操作
+                localStorage.clear();
+                break;
+            case 403:
+                Errmessage = '拒绝访问(403)';
+                localStorage.clear();
+                break;
+            case 404:
+                Errmessage = '请求出错(404)';
+                localStorage.clear();
+                break;
+            case 408:
+                Errmessage = '请求超时(408)';
+                break;
+            case 500:
+                Errmessage = '服务器错误(500)';
+                break;
+            case 501:
+                Errmessage = '服务未实现(501)';
+                break;
+            case 502:
+                Errmessage = '网络错误(502)';
+                break;
+            case 503:
+                Errmessage = '服务不可用(503)';
+                break;
+            case 504:
+                Errmessage = '网络超时(504)';
+                break;
+            case 505:
+                Errmessage = 'HTTP版本不受支持(505)';
+                break;
+            default:
+                Errmessage = `连接出错(${err.response.status})!`;
+        }
+        message.error(Errmessage)
+        return Promise.reject(err.response)
     }
 )
 
